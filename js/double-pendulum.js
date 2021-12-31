cd1e356fc8062689af3912243ae10bb7372cb2fc
Alan <alantrawcz@gmail.com>
2021-12-31 11:32:36 +0100

    pendulum as class object

---
+++ js/double-pendulum.js
@@ -0,0 +1,142 @@
+
+class DoublePendulum {
+	constructor(r1, r2, m1, m2, a1, a2, g) {
+		// Radius (m)
+		this.r1 = r1;
+		this.r2 = r2;
+		// Mass (g)
+		this.m1 = m1;
+		this.m2 = m2;
+		// Angle (rad)
+		this.a1 = a1;
+		this.a2 = a2;
+		// Angular velocity
+		this.v1 = 0;
+		this.v2 = 0;
+		// Gravity
+		this.g = g;
+	}
+
+	// Pedulums coordinates
+	get x1() {
+		return this.r1 * Math.sin(this.a1);
+	}
+	get y1() {
+		return this.r1 * Math.cos(this.a1);
+	}
+	get x2() {
+		return this.x1 + this.r2 * Math.sin(this.a2);
+	}
+	get y2() {
+		return this.y1 + this.r2 * Math.cos(this.a2);
+	}
+
+	// Energies
+	get mechanicalE() {
+		return this.kineticE - this.potentialE;
+	}
+
+	get kineticE() {
+		return (1 / 2) * this.m1 * this.r1 * this.r1 * this.v1 * this.v1 + (1 / 2) * this.m2 * (this.r1 * this.r1 * this.v1 * this.v1 + this.r2 * this.r2 * this.v2 * this.v2 + 2 * this.r1 * this.r2 * this.v1 * this.v2 * Math.cos(this.a1 - this.a2));
+	}
+
+	get potentialE() {
+		return -(this.m1 + this.m2) * g * this.r1 * Math.cos(this.a1) - this.m2 * g * this.r2 * Math.cos(this.a2);
+	}
+
+
+	// Numerical approximation methods
+	runForwardEuler() {
+		const [acc1, acc2] = this._eulerAccs();
+
+		this.a1 += this.v1;
+		this.a2 += this.v2;
+
+		this.v1 += acc1;
+		this.v2 += acc2;
+	}
+
+
+	runBackwardsEuler() {
+		const [acc1, acc2] = this._eulerAccs();
+
+		this.v1 += acc1;
+		this.v2 += acc2;
+
+		this.a1 += this.v1;
+		this.a2 += this.v2;
+	}
+
+
+	_eulerAccs() {
+		let
+			num1 = -this.g * (2 * this.m1 + this.m2) * Math.sin(this.a1),
+			num2 = -this.m2 * this.g * Math.sin(this.a1 - 2 * this.a2),
+			num3 = -2 * Math.sin(this.a1 - this.a2) * this.m2,
+			num4 = this.v2 * this.v2 * this.r2 + this.v1 * this.v1 * this.r1 * Math.cos(this.a1 - this.a2),
+			den = this.r1 * (2 * this.m1 + this.m2 - this.m2 * Math.cos(2 * this.a1 - 2 * this.a2)),
+			acc1 = (num1 + num2 + num3 * num4) / den,
+			acc2;
+
+		num1 = 2 * Math.sin(this.a1 - this.a2);
+		num2 = this.v1 * this.v1 * this.r1 * (this.m1 + this.m2);
+		num3 = this.g * (this.m1 + this.m2) * Math.cos(this.a1);
+		num4 = this.v2 * this.v2 * this.r2 * this.m2 * Math.cos(this.a1 - this.a2);
+		den = this.r2 * (2 * this.m1 + this.m2 - this.m2 * Math.cos(2 * this.a1 - 2 * this.a2));
+		acc2 = (num1 * (num2 + num3 + num4)) / den;
+
+		return [acc1, acc2]
+	}
+
+
+	// Falta definir `dt`
+	runRK4(dt) {
+		let l = [this.a1, this.a2, this.v1, this.v2],
+    k1,k2,k3,k4,ret;
+
+		k1 = this._lagrange(l);
+
+		k2 = this._lagrange(l.map((x, i) =>
+			x + ( k1[i] / 2)
+		));
+
+		k3 = this._lagrange(l.map((x, i) =>
+			x + ( k2[i] / 2)
+		));
+
+		k4 = this._lagrange(l.map((x, i) =>
+			x + k3[i]
+		));
+
+		ret = k1.map((x, i) =>
+			(1 / 6) * (x + 2 * k2[i] + 2 * k3[i] + k4[i])
+		);
+
+		this.a1 += ret[0];
+		this.a2 += ret[1];
+		this.v1 += ret[2];
+		this.v2 += ret[3];
+	}
+
+
+	_lagrange(f) {
+		let [t1, t2, w1, w2] = f,
+    delta_t = t1-t2;
+
+    // let f0 = ((this.m2 * this.r1 * w1**2 * Math.sin(2 * delta_t)) + (2 * this.m2 * this.r2 * w2**2 * Math.sin(delta_t)) + (2 * this.g * this.m2 * Math.cos(t2) * Math.sin(delta_t)) + (2 * this.g * this.m1 * Math.sin(t1)) ) / (-2 * this.r1 * (this.m1 + this.m2 * (Math.sin(delta_t)**2)));
+
+    // let f1 = ((this.m2 * this.r2 * w2**2 * Math.sin(2*delta_t)) + (2 * (this.m1 + this.m2) * this.r1 * w2**2 *Math.sin(delta_t)) + (2 * this.g * (this.m1 + this.m2) * Math.cos(t1) * Math.sin(delta_t))) / ( 2 * this.r2 * (this.m1 + (this.m2 * Math.sin(delta_t)**2)));
+
+		let an1 = (this.r2 / this.r1) * (this.m2 / (this.m1 + this.m2)) * Math.cos(t1 - t2);
+		let an2 = (this.r1 / this.r2) * Math.cos(t1 - t2);
+
+		let f1 = -(this.r2 / this.r1) * (this.m2 / (this.m1 + this.m2)) * (w2 ** 2) * Math.sin(t1 - t2) - (this.g / this.r1) * Math.sin(t1);
+		let f2 = (this.r1 / this.r2) * (w1 * w1) * Math.sin(t1 - t2) - (this.g / this.r2) * Math.sin(t2);
+
+		let g1 = (f1 - an1 * f2) / (1 - an1 * an2);
+		let g2 = (f2 - an2 * f1) / (1 - an1 * an2);
+
+		return [w1, w2, g1, g2];
+	}
+
+}
---
+++ js/sketch.js
@@ -0,0 +1,210 @@
+
+const canvasContainer = $('#canvasContainer')[0],
+	rsButton = $('#toggleExecButton')[0];
+
+
+let
+	// true if canvas is executing
+	running,
+	// trace renderer
+	trace,
+	// translate
+	tx, ty,
+	// pendulum
+	pendulum = new DoublePendulum(
+		2,											// r1
+		2.5,										// r2
+		0.05,										// m1
+		0.05,										// m2
+		80 * (Math.PI / 180),		// a1
+		80 * (Math.PI / 180),		// a2
+		9.8 / (60 ** 2) 				// g
+	),
+
+	// modes (0: line, else: dot)
+	traceMode = 0,
+	// colors
+	backgroundColor,
+	pendulumColor,
+	traceColor,
+	// weigths
+	pendulumWeight = 0.1,
+	traceWeight = 0.1,
+
+	scaleFactor,
+
+	// previous pendulum position
+	x2prev = null,
+	y2prev = null
+  // Runge Kutta 4
+  // Tiempo inicial y máximo
+  t0 = 0,
+  t = 60*10,
+  // Particiones del tiempo
+  n = 60*60*24,
+  // Paso del tiempo (dt)
+  h = (t-t0)/n;
+  ;
+
+
+function setColors() {
+	backgroundColor = color(0);
+	pendulumColor = color(150);
+	traceColor = color('hsl(160, 100%, 50%)');
+}
+
+
+// P5
+// -----
+function setup() {
+	initInterface();
+
+	const canvas = createCanvas();
+	canvas.parent(canvasContainer);
+	resizeCanvas(canvasContainer.clientWidth, canvasContainer.clientHeight);
+	scaleFactor = min(width, height) / (2 * (pendulum.r1 + pendulum.r2));
+
+	// frameRate(30);
+	setColors();
+	tx = width / 2;
+	ty = height / 2;
+	initTrace();
+	// stop the execution
+	running = false;
+}
+
+
+function initTrace() {
+	trace = createGraphics(width, height);
+	trace.translate(tx, ty);
+	trace.scale(scaleFactor);
+}
+
+
+
+function draw() {
+	background(backgroundColor);
+	image(trace, 0, 0);
+	translate(tx, ty);
+	scale(scaleFactor);
+
+	if (running) {
+		// pendulum.runForwardEuler();
+    runRK4();
+		drawTrace(pendulum);
+	}
+	drawPendulum(pendulum);
+}
+
+
+function drawTrace(p) {
+	trace.stroke(traceColor);
+
+	if (traceMode) {
+		trace.circle(p.x2, p.y2, traceWeight);
+	}
+	else {
+		if (x2prev !== null) {
+			trace.strokeWeight(traceWeight);
+			trace.line(x2prev, y2prev, p.x2, p.y2);
+		}
+		x2prev = p.x2;
+		y2prev = p.y2;
+	}
+}
+
+
+function drawPendulum(p) {
+	strokeWeight(pendulumWeight);
+	stroke(pendulumColor);
+	fill(pendulumColor);
+
+	// pendulum 1
+	line(0, 0, p.x1, p.y1);
+	circle(p.x1, p.y1, p.m1);
+
+	// pendulum 2
+	line(p.x1, p.y1, p.x2, p.y2);
+	circle(p.x2, p.y2, p.m2);
+}
+
+// Funtion to execute RK4 model
+function runRK4(){
+  pendulum.runRK4(t0)
+  console.log(`t0 = ${t0}`)
+  // time update
+  t0 = t0+h;
+  
+}
+
+
+// Events
+// --------
+
+function initInterface() {
+	$('.slider').each(function () {
+		this.value = eval(`pendulum.${this.name}`);
+		$(`#${this.name}Value`).text(this.value);
+	});
+
+	addEventListeners();
+}
+
+function addEventListeners() {
+	rsButton.addEventListener('click', toggleExec);
+	resetButton.addEventListener('click', () => location.reload());
+	$('.slider').on("input", controlSliderInput);
+}
+
+
+function controlSliderInput(e) {
+	const slider = e.target,
+		sliderType = slider.id[0],
+		valueContainer = $("#" + slider.id.substring(0, 2) + "Value")[0],
+		varName = slider.name;
+
+	switch (sliderType) {
+		case 'r':
+			controlRadiusInput(slider, valueContainer, varName);
+			break;
+		case 'a':
+			controlAngleInput(slider, valueContainer, varName);
+			break;
+		case 'm':
+			controlMassInput(slider, valueContainer, varName);
+			break;
+	}
+}
+
+function controlRadiusInput(slider, valueContainer, varName) {
+	valueContainer.innerHTML = slider.value;
+	eval(`pendulum.${varName} = ${slider.value}`);
+	updateScaleFactor();
+}
+
+function controlAngleInput(slider, valueContainer, varName) {
+	valueContainer.innerHTML = slider.value;
+	eval(`pendulum.${varName} = ${radians(slider.value)}`);
+}
+
+function controlMassInput(slider, valueContainer, varName) {
+	valueContainer.innerHTML = slider.value;
+	eval(`pendulum.${varName} = ${slider.value / 1000}`);
+}
+
+function updateScaleFactor() {
+	scaleFactor = min(width, height) / (2 * (pendulum.r1 + pendulum.r2));
+	initTrace();
+}
+
+
+function toggleExec() {
+	running = !running;
+	disableInputs();
+}
+
+
+function disableInputs() {
+	// document.querySelectorAll('input').forEach((input) => input.style.display = 'none');
+}
+
